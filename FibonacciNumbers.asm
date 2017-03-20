8dc44a25177d2c403c22aa3896af3db8fa8b980f
Kelsey Helms <kelseyhelms@gmail.com>
2017-03-19 18:33:01 -0700

    Update FibonacciNumbers.asm

--- FibonacciNumbers.asm
+++ FibonacciNumbers.asm
@@ -42,7 +42,7 @@ count2			DWORD	36
 
 main PROC
 
-;----------------------------------------------------------------------------------------------introduce programmer
+;------------------------------------------------introduce programmer
 	mov	edx, OFFSET intro
 	call	WriteString
 	call	CrLf
@@ -51,14 +51,14 @@ main PROC
 	call	CrLf
 	call	CrLf
 
-;----------------------------------------------------------------------------------------------prompt user to enter name
+;------------------------------------------------prompt user to enter name
 	mov	edx, OFFSET getName
 	call	WriteString
-;----------------------------------------------------------------------------------------------read in user's name
+;------------------------------------------------read in user's name
 	mov	edx, OFFSET userName
 	mov	ecx, 32
 	call	ReadString
-;----------------------------------------------------------------------------------------------say hello to user using their name
+;------------------------------------------------say hello to user using their name
 	mov	edx, OFFSET hello
 	call	WriteString
 	mov	edx, OFFSET userName
@@ -66,127 +66,127 @@ main PROC
 	call	CrLf
 	call	CrLf
 
-;----------------------------------------------------------------------------------------------introduce program concept
+;------------------------------------------------introduce program concept
 	mov	edx, OFFSET intro2
 	call	WriteString
 	call	CrLf
-;----------------------------------------------------------------------------------------------introduce range
+;------------------------------------------------introduce range
 	mov	edx, OFFSET range
 	call	WriteString
 	call	CrLf
 	call	CrLf
 
-;----------------------------------------------------------------------------------------------prompt user for number
+;------------------------------------------------prompt user for number
 	mov	edx, OFFSET getNum
 	call	WriteString
-;----------------------------------------------------------------------------------------------read input as inputNum
+;------------------------------------------------read input as inputNum
 	call	ReadInt	
 	mov	inputNum, eax
 
 
-;----------------------------------------------------------------------------------------------Check range
+;------------------------------------------------Check range
 CheckRange :
-;----------------------------------------------------------------------------------------------compare against low end and jump if too low
+;------------------------------------------------compare against low end and jump if too low
 	mov	eax, inputNum
 	cmp	eax, 1	
 	jl	OutsideRange
-;----------------------------------------------------------------------------------------------compare against high end and jump is too high
+;------------------------------------------------compare against high end and jump is too high
 	cmp	eax, 46	
 	jg	OutsideRange
-;----------------------------------------------------------------------------------------------skip if in range
+;------------------------------------------------skip if in range
 	jmp	InRange
 
 
-;----------------------------------------------------------------------------------------------if the number is out of range, re-enter number
+;------------------------------------------------if the number is out of range, re-enter number
 OutsideRange :
-;----------------------------------------------------------------------------------------------warn user out of range
+;------------------------------------------------warn user out of range
 	mov	edx, OFFSET outOfRange
 	call	WriteString
 	call	CrLf
-;----------------------------------------------------------------------------------------------prompt user for number
+;------------------------------------------------prompt user for number
 	mov	edx, OFFSET getNum
 	call	WriteString
-;----------------------------------------------------------------------------------------------read input as inputNum
+;------------------------------------------------read input as inputNum
 	call	ReadInt	
 	mov	inputNum, eax
 	jmp	CheckRange
 
 
-;----------------------------------------------------------------------------------------------if number is in range, continue forward
+;------------------------------------------------if number is in range, continue forward
 InRange :
-;----------------------------------------------------------------------------------------------set up amount of loops
+;------------------------------------------------set up amount of loops
 	mov	ecx, inputNum
 
 
-;----------------------------------------------------------------------------------------------fibonacci terms
+;------------------------------------------------fibonacci terms
 Terms:
-;----------------------------------------------------------------------------------------------display number, 1 the first time, or result
+;------------------------------------------------display number, 1 the first time, or result
 	mov	eax, term2
 	call	WriteDec
 	
-;----------------------------------------------------------------------------------------------calculate next term
+;------------------------------------------------calculate next term
 	mov	eax, term1
 	add	eax, term2
-;----------------------------------------------------------------------------------------------move result into tempterm
+;------------------------------------------------move result into tempterm
 	mov	tempterm, eax
-;----------------------------------------------------------------------------------------------shift 2nd term to 1st term
+;------------------------------------------------shift 2nd term to 1st term
 	mov	eax, term2
 	mov	term1, eax
-;----------------------------------------------------------------------------------------------shift result to 2nd term
+;------------------------------------------------shift result to 2nd term
 	mov	eax, tempterm
 	mov	term2, eax
-;----------------------------------------------------------------------------------------------horizontal tab for spacing
+;------------------------------------------------horizontal tab for spacing
 	mov     al, TAB
    	call    WriteChar
 
-;----------------------------------------------------------------------------------------------because if more than 36 terms are displayed, tabs warp columns, see if there are more than 36 terms to be displayed.
+;------------------------------------------------because if more than 36 terms are displayed, tabs warp columns, see if there are more than 36 terms to be displayed.
 	sub	count2, 1
 	cmp	count2, 0
-;----------------------------------------------------------------------------------------------if so, add a second tab to first 36 terms, then only one tab after
+;------------------------------------------------if so, add a second tab to first 36 terms, then only one tab after
 	je	CheckTabs
-;----------------------------------------------------------------------------------------------horizontal tab for spacing
+;------------------------------------------------horizontal tab for spacing
 	mov     al, TAB
     	call    WriteChar
 
 
-;----------------------------------------------------------------------------------------------check tabs
+;------------------------------------------------check tabs
 CheckTabs :			
-;----------------------------------------------------------------------------------------------if reached 36 terms, keep count2 at zero to continue skipping second tab
+;------------------------------------------------if reached 36 terms, keep count2 at zero to continue skipping second tab
 	cmp	count2, 0	
 	je	AddOne	
-;----------------------------------------------------------------------------------------------if haven't reached 36 terms, skip addition to continue second tab
+;------------------------------------------------if haven't reached 36 terms, skip addition to continue second tab
 	jmp	CheckColumns	
 
 
-;----------------------------------------------------------------------------------------------add one
+;------------------------------------------------add one
 AddOne :
-;----------------------------------------------------------------------------------------------add one to count2 to keep at zero
+;------------------------------------------------add one to count2 to keep at zero
 	add	count2, 1
 
 
-;----------------------------------------------------------------------------------------------check columns
+;------------------------------------------------check columns
 CheckColumns :
-;----------------------------------------------------------------------------------------------if reached four terms, start new line
+;------------------------------------------------if reached four terms, start new line
 	sub	count, 1
 	cmp	count, 0
-;----------------------------------------------------------------------------------------------else, skip new line and display next term, start next calculation
+;------------------------------------------------else, skip new line and display next term, start next calculation
 	jne	LoopAgain
-;----------------------------------------------------------------------------------------------if reached four terms, start count to four over again
+;------------------------------------------------if reached four terms, start count to four over again
 	mov	count, 4
 	call	CrLf
-;----------------------------------------------------------------------------------------------loops as many times as user wanted
+;------------------------------------------------loops as many times as user wanted
 LoopAgain :
 	loop	Terms
 
 
-;----------------------------------------------------------------------------------------------Say goodbye
+;------------------------------------------------Say goodbye
 	call	CrLf				
 	call	CrLf
-;----------------------------------------------------------------------------------------------verify results with programmer name
+;------------------------------------------------verify results with programmer name
 	mov	edx, OFFSET goodbye1
 	call	WriteString
 	call	CrLf
-;----------------------------------------------------------------------------------------------say goodbye with user's name
+;------------------------------------------------say goodbye with user's name
 	mov	edx, OFFSET goodbye2
 	call	WriteString
 	mov	edx, OFFSET userName
@@ -195,9 +195,8 @@ LoopAgain :
 	call	CrLf
 
 
-;----------------------------------------------------------------------------------------------exit to operating system
+;------------------------------------------------exit to operating system
 	exit
-	
 main ENDP
 
 END main
