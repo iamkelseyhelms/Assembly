f6d8e034158659b0e8133b8d2c5cd50fc65a7eeb
Kelsey Helms <kelseyhelms@gmail.com>
2017-03-19 18:08:21 -0700

    Update ElementaryArithmetic.asm

--- ElementaryArithmetic.asm
+++ ElementaryArithmetic.asm
@@ -11,175 +11,189 @@ INCLUDE Irvine32.inc
 
 .data
 
+;----------------------------------------------------------------------------------------------messages
 intro			BYTE	"	Elementary Arithmetic		by Kelsey Helms", 0
 intro2			BYTE	"**EC: Program repeats until user chooses to quit.", 0
 intro3			BYTE	"**EC: Program verifies second number is less than first.", 0
 intro4			BYTE	"Enter 2 numbers, and I'll show you the sum, difference, product, quotient, and remainder.", 0
 prompt1			BYTE	"First number: ", 0
 prompt2			BYTE	"Second number: ", 0
-number1			DWORD	?
-number2			DWORD	?
-sum				DWORD	?
-difference		DWORD	?
-product			DWORD	?
-quotient		DWORD	?
-remainder		DWORD	?
+remain			BYTE	" remainder ", 0
+negative		BYTE	"The second number must be less than the first!", 0
+repetition1		BYTE	"Press any key to quit program ", 0
+goodbye			BYTE	"Impressed? Bye!", 0
+;----------------------------------------------------------------------------------------------math
 plus			BYTE	" + ", 0
 subtract		BYTE	" - ", 0
 times			BYTE	" x ", 0
 divide			BYTE	" / ", 0
 equals			BYTE	" = ", 0
-remain			BYTE	" remainder ", 0
-negative		BYTE	"The second number must be less than the first!", 0
-repetition1		BYTE	"Press any key to quit program ", 0
+;----------------------------------------------------------------------------------------------variables
+number1			DWORD	?
+number2			DWORD	?
+sum			DWORD	?
+difference		DWORD	?
+product			DWORD	?
+quotient		DWORD	?
+remainder		DWORD	?
 char			BYTE	?
-goodbye			BYTE	"Impressed? Bye!", 0
+
 
 .code
+
+;-----------------------------------------------;
+; PROC:		main				;
+; PURPOSE:	runs program 			;
+; PARAMETERS:	none				;
+; RETURNS:	none				;
+;-----------------------------------------------;
+
 main PROC
 
-;Introduce programmer
-	mov		edx, OFFSET intro
+;----------------------------------------------------------------------------------------------Introduce programmer
+	mov	edx, OFFSET intro
 	call	WriteString
 	call	CrLf
-	mov		edx, OFFSET intro2
+	mov	edx, OFFSET intro2
 	call	WriteString
 	call	CrLf
-	mov		edx, OFFSET intro3
+	mov	edx, OFFSET intro3
 	call	WriteString
 	call	CrLf
 	call	CrLf
-	mov		edx, OFFSET intro4
+	mov	edx, OFFSET intro4
 	call	WriteString
 
+;----------------------------------------------------------------------------------------------Get numbers from user
 Top :
-;Get numbers from user
 	call	CrLf
-		call	Crlf
-	mov		edx, OFFSET prompt1
+	call	Crlf
+	mov	edx, OFFSET prompt1
 	call	WriteString
 	call	ReadInt
-	mov		number1, eax
-	mov		edx, OFFSET prompt2
+	mov	number1, eax
+	mov	edx, OFFSET prompt2
 	call	WriteString
 	call	ReadInt
-	mov		number2, eax
+	mov	number2, eax
 	call	CrLf
 
-;Test if number1 is greater than number2
-	mov		eax, number1
-	cmp		eax, number2
-	jl		NegSubtraction
+;----------------------------------------------------------------------------------------------Test if number1 is greater than number2
+	mov	eax, number1
+	cmp	eax, number2
+	jl	NegSubtraction
 
-;Calculate results
-;Add
-	mov		eax, number1
-	add		eax, number2
-	mov		sum, eax
+;----------------------------------------------------------------------------------------------Calculate results
 
-;Subtract
-	mov		eax, number1
-	sub		eax, number2
-	mov		difference, eax
+;----------------------------------------------------------------------------------------------Add
+	mov	eax, number1
+	add	eax, number2
+	mov	sum, eax
 
-;Multiply
-	mov		eax, number1
-	mov		ebx, number2
-	mul		ebx
-	mov		product, eax
+;----------------------------------------------------------------------------------------------Subtract
+	mov	eax, number1
+	sub	eax, number2
+	mov	difference, eax
 
-;Divide
-	mov		edx, 0
-	mov		eax, number1
-	mov		ebx, number2
-	div		ebx
-	mov		quotient, eax
-	mov		remainder, edx
+;----------------------------------------------------------------------------------------------Multiply
+	mov	eax, number1
+	mov	ebx, number2
+	mul	ebx
+	mov	product, eax
+
+;----------------------------------------------------------------------------------------------Divide
+	mov	edx, 0
+	mov	eax, number1
+	mov	ebx, number2
+	div	ebx
+	mov	quotient, eax
+	mov	remainder, edx
 
 
-;Display results
+;----------------------------------------------------------------------------------------------Display results
 
-;Add
-	mov		eax, number1
+;----------------------------------------------------------------------------------------------Add
+	mov	eax, number1
 	call	WriteDec
-	mov		edx, OFFSET plus
+	mov	edx, OFFSET plus
 	call	WriteString
-	mov		eax, number2
+	mov	eax, number2
 	call	WriteDec
-	mov		edx, OFFSET	equals
+	mov	edx, OFFSET equals
 	call	WriteString
-	mov		eax, sum
+	mov	eax, sum
 	call	WriteDec
 	call	CrLf
-;Subtract
-	mov		eax, number1
+;----------------------------------------------------------------------------------------------Subtract
+	mov	eax, number1
 	call	WriteDec
-	mov		edx, OFFSET subtract
+	mov	edx, OFFSET subtract
 	call	WriteString
-	mov		eax, number2
+	mov	eax, number2
 	call	WriteDec
-	mov		edx, OFFSET	equals
+	mov	edx, OFFSET equals
 	call	WriteString
-	mov		eax, difference
+	mov	eax, difference
 	call	WriteDec
 	call	CrLf
-;Multiply	
-	mov		eax, number1
+;----------------------------------------------------------------------------------------------Multiply	
+	mov	eax, number1
 	call	WriteDec
-	mov		edx, OFFSET times
+	mov	edx, OFFSET times
 	call	WriteString
-	mov		eax, number2
+	mov	eax, number2
 	call	WriteDec
-	mov		edx, OFFSET	equals
+	mov	edx, OFFSET equals
 	call	WriteString
-	mov		eax, product
+	mov	eax, product
 	call	WriteDec
 	call	CrLf
-;Divide	
-	mov		eax, number1
+;----------------------------------------------------------------------------------------------Divide	
+	mov	eax, number1
 	call	WriteDec
-	mov		edx, OFFSET divide
+	mov	edx, OFFSET divide
 	call	WriteString
-	mov		eax, number2
+	mov	eax, number2
 	call	WriteDec
-	mov		edx, OFFSET	equals
+	mov	edx, OFFSET equals
 	call	WriteString
-	mov		eax, quotient
+	mov	eax, quotient
 	call	WriteDec
-	mov		edx, OFFSET remain
+	mov	edx, OFFSET remain
 	call	WriteString
-	mov		eax, remainder
+	mov	eax, remainder
 	call	WriteDec
 	call	CrLf
 	call	CrLf
-	jmp		GoToEnd
+	jmp	GoToEnd
 
+;----------------------------------------------------------------------------------------------Warn user number1 must be greater than number2
 NegSubtraction :
-	mov		edx, OFFSET negative
+	mov	edx, OFFSET negative
 	call	WriteString
 	call	CrLf
 	call	CrLf
 
+;----------------------------------------------------------------------------------------------Ask user if they want to repeat
 GoToEnd :
-;Ask user if they want to repeat
-	mov		edx, OFFSET repetition1
+	mov	edx, OFFSET repetition1
 	call	WriteString
-	mov		eax, 5000
+	mov	eax, 5000
 	call	Delay
 	call	ReadKey
-	jz		Top
-	mov		char, AL
+	jz	Top
+	mov	char, AL
 	
-;Say goodbye
+;----------------------------------------------------------------------------------------------Say goodbye
 	call	CrLf
 	call	CrLf
-	mov		edx, OFFSET goodbye
+	mov	edx, OFFSET goodbye
 	call	WriteString
 	call	CrLf
 
-	exit	; exit to operating system
+;----------------------------------------------------------------------------------------------exit to operating system
+	exit
+	
 main ENDP
-
-; (insert additional procedures here)
 
 END main
