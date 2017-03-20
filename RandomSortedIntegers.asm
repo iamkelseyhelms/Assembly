a550402f8cb9de4c6fea362a8014a486647ea46b
Kelsey Helms <kelseyhelms@gmail.com>
2017-03-19 18:43:25 -0700

    Update RandomSortedIntegers.asm

--- RandomSortedIntegers.asm
+++ RandomSortedIntegers.asm
@@ -6,7 +6,7 @@ TITLE Random Sorted Integers    (Program04.asm)
 ; Project ID: Program04
 ; Due date: July 27, 2016                 
 ; Description: This program will introduce the programmer, get an amount from the user, and validate the amount is within range.
-;				It will then produce that many random integers, find the median, and reproduce the same integers in sorted descending order.
+;It will then produce that many random integers, find the median, and reproduce the same integers in sorted descending order.
 
 INCLUDE Irvine32.inc
 
@@ -38,10 +38,10 @@ INCLUDE Irvine32.inc
 .code
 
 ;-----------------------------------------------;
-; PROC:			main							;
-; PURPOSE:		runs program through call list	;
-; PARAMETERS:	none							;
-; RETURNS:		none							;
+; PROC:		main				;
+; PURPOSE:	runs program through call list	;
+; PARAMETERS:	none				;
+; RETURNS:	none				;
 ;-----------------------------------------------;
 
 main PROC
@@ -84,299 +84,300 @@ main PROC
 	push	OFFSET goodbye2
 	call	farewell
 
-	exit	; exit to operating system
+;------------------------------------------------exit to operating system
+	exit
 main ENDP
 
 
 ;-----------------------------------------------;
-; PROC:			introduction					;
-; PURPOSE:		introduce program				;
+; PROC:		introduction			;
+; PURPOSE:	introduce program		;
 ; PARAMETERS:	intro, intro1, intro2, intro3	;
-; RETURNS:		none							;
+; RETURNS:	none				;
 ;-----------------------------------------------;
 
 introduction PROC
 ;------------------------------------------------set up stack frame
 	push	ebp
-	mov		ebp, esp
+	mov	ebp, esp
 ;------------------------------------------------access intro and display
-	mov		edx, [ebp+20]
+	mov	edx, [ebp+20]
 	call	WriteString
 	call	CrLf
 ;------------------------------------------------access intro1 and display
-	mov		edx, [ebp+16]
+	mov	edx, [ebp+16]
 	call	WriteString
 	call	CrLf
 ;------------------------------------------------access intro2 and display
-	mov		edx, [ebp+12]
+	mov	edx, [ebp+12]
 	call	WriteString
 	call	CrLf
 ;------------------------------------------------access intro3 and display
-	mov		edx, [ebp+8]
+	mov	edx, [ebp+8]
 	call	WriteString
 	call	CrLf
 	call	CrLf
 ;------------------------------------------------reset ebp and return with 4 messages
-	pop		ebp
-	ret		16
+	pop	ebp
+	ret	16
 
 introduction ENDP
 
 
 ;-----------------------------------------------;
-; PROC:			getUserData						;
-; PURPOSE:		gets input from user			;
+; PROC:		getUserData			;
+; PURPOSE:	gets input from user		;
 ; PARAMETERS:	instruct, userNum, outOfRange	;
-; RETURNS:		filled userNum					;
+; RETURNS:	filled userNum			;
 ;-----------------------------------------------;
 
 getUserData PROC
 ;------------------------------------------------set up stack frame
 	push	ebp
-	mov		ebp, esp
+	mov	ebp, esp
 ;------------------------------------------------access instruct and display
 GetNum:
-	mov		edx, [ebp+16]
+	mov	edx, [ebp+16]
 	call	WriteString
 ;------------------------------------------------read user input and store in userNum variable in stack
 	call	ReadInt
-	mov		ebx, [ebp+12]
-	mov		[ebx], eax
+	mov	ebx, [ebp+12]
+	mov	[ebx], eax
 ;------------------------------------------------validate user input
-	cmp		eax, MIN
-	jl		OutsideRange
-	cmp		eax, MAX
-	jg		OutsideRange
+	cmp	eax, MIN
+	jl	OutsideRange
+	cmp	eax, MAX
+	jg	OutsideRange
 ;------------------------------------------------reset ebp and return with 2 messages and 1 variable
 	call	CrLf
-	pop		ebp
-	ret		12
+	pop	ebp
+	ret	12
 ;------------------------------------------------access outOfRange and display, then re-start GetNum
 OutsideRange:
 	call	CrLf
-	mov		edx, [ebp+8]
+	mov	edx, [ebp+8]
 	call	WriteString
 	call	CrLf
-	jmp		GetNum
+	jmp	GetNum
 
 getUserData ENDP
 
 
 ;-----------------------------------------------;
-; PROC:			fillArray						;
-; PURPOSE:		fills array with random numbers	;
-; PARAMETERS:	userArray, userNum				;
-; RETURNS:		filled userArray				;
+; PROC:		fillArray			;
+; PURPOSE:	fills array with random numbers	;
+; PARAMETERS:	userArray, userNum		;
+; RETURNS:	filled userArray		;
 ;-----------------------------------------------;
 
 fillArray PROC
 ;------------------------------------------------set up stack frame
 	push	ebp
-	mov		ebp, esp
+	mov	ebp, esp
 ;------------------------------------------------set up userNum as loop count, set up userArray and randomization
-	mov		edi, [ebp+12]
-	mov		ecx, [ebp+8]
+	mov	edi, [ebp+12]
+	mov	ecx, [ebp+8]
 	call	Randomize
 ;------------------------------------------------get random number within range of 100-999
 Next:
-	mov		eax, hi				 
-	sub		eax, lo
-	inc		eax
+	mov	eax, hi				 
+	sub	eax, lo
+	inc	eax
 	call	RandomRange
-	add		eax, lo
+	add	eax, lo
 ;------------------------------------------------put number in array, increment array element, and repeat
-	mov		[edi], eax
-	add		edi, 4
+	mov	[edi], eax
+	add	edi, 4
 	loop	Next
 ;------------------------------------------------reset ebp and return with 2 variables
-	pop		ebp
-	ret		8
+	pop	ebp
+	ret	8
 
 fillArray ENDP
 
 
 ;-----------------------------------------------;
-; PROC:			sortList						;
-; PURPOSE:		sorts array in descending order	;
-; PARAMETERS:	userArray, userNum				;
-; RETURNS:		sorted userArray				;
+; PROC:		sortList			;
+; PURPOSE:	sorts array in descending order	;
+; PARAMETERS:	userArray, userNum		;
+; RETURNS:	sorted userArray		;
 ;-----------------------------------------------;
 
 sortList PROC
 ;------------------------------------------------set up stack frame
 	push	ebp
-	mov		ebp, esp
+	mov	ebp, esp
 ;------------------------------------------------set up userNum as loop count
-	mov		ecx, [ebp+8]
-	dec		ecx
+	mov	ecx, [ebp+8]
+	dec	ecx
 ;------------------------------------------------initilize outer loop with userArray at beginning, save OuterLoop count
 OuterLoop:			
-	mov		esi, [ebp+12]
+	mov	esi, [ebp+12]
 	push 	ecx	
 ;------------------------------------------------InnerLoop compares two neighboring elements (x, x+1)
 InnerLoop:
-	mov		eax, [esi]
-	cmp		eax, [esi+4]
+	mov	eax, [esi]
+	cmp	eax, [esi+4]
 ;------------------------------------------------skip exchange if x is greater than x+1
-	jg		LoopInnLoop
+	jg	LoopInnLoop
 ;------------------------------------------------if x is less than x+1, pass both elements as arguments to exchange procedure
 	push	[esi]
 	push	[esi+4]
 	call 	exchange
 ;------------------------------------------------receive both elements back, now switched
-	pop		[esi+4]
-	pop		[esi]
+	pop	[esi+4]
+	pop	[esi]
 ;------------------------------------------------repeat InnerLoop
 LoopInnLoop:
-	add		esi, 4
+	add	esi, 4
 	loop	InnerLoop
 ;------------------------------------------------get OuterLoop count and repeat OuterLoop
 	pop 	ecx
 	loop 	OuterLoop
 ;------------------------------------------------reset ebp and return with 2 variables
-	pop		ebp
+	pop	ebp
 	ret 	8
 
 sortList ENDP
 
 
 ;-----------------------------------------------;
-; PROC:			exchange						;
-; PURPOSE:		swaps two elements				;
-; PARAMETERS:	element x, element x+1			;
-; RETURNS:		element x, element x+1 swapped	;
+; PROC:		exchange			;
+; PURPOSE:	swaps two elements		;
+; PARAMETERS:	element x, element x+1		;
+; RETURNS:	element x, element x+1 swapped	;
 ;-----------------------------------------------;
 
 exchange PROC
 ;------------------------------------------------set up stack frame and push all registers to preserve sortList
 	push	ebp
-	mov		ebp, esp
+	mov	ebp, esp
 ;------------------------------------------------move element[x] in temp
-	mov		ebx, [ebp+12] 		
+	mov	ebx, [ebp+12] 		
 ;------------------------------------------------move element[x+1] in temp
 	mov 	edx, [ebp+8]
 ;------------------------------------------------swap
-	mov		[ebp+12], edx
-	mov		[ebp+8], ebx
+	mov	[ebp+12], edx
+	mov	[ebp+8], ebx
 ;------------------------------------------------reset ebp and return
-	pop		ebp
+	pop	ebp
 	ret 	
 exchange ENDP
 
 
 ;-----------------------------------------------;
-; PROC:			displayMedian					;
-; PURPOSE:		displays array median			;
-; PARAMETERS:	title, userArray, userNum		;
-; RETURNS:		none							;
+; PROC:		displayMedian			;
+; PURPOSE:	displays array median		;
+; PARAMETERS:	title, userArray, userNum	;
+; RETURNS:	none				;
 ;-----------------------------------------------;
 
 displayMedian PROC
 ;------------------------------------------------set up stack frame
 	push	ebp
-	mov		ebp, esp
+	mov	ebp, esp
 ;------------------------------------------------access title and display
-	mov		edx, [ebp+16]
+	mov	edx, [ebp+16]
 	call	WriteString
 	call	CrLf
 ;------------------------------------------------set up userArray, userNum, and divisor
-	mov		edi, [ebp+12]
-	mov		eax, [ebp+8]
+	mov	edi, [ebp+12]
+	mov	eax, [ebp+8]
 	cdq
-	mov		ebx, 2
+	mov	ebx, 2
 ;------------------------------------------------find the middle of userArray
-	div		ebx
-	shl		eax, 2
-	add		edi, eax
-	mov		eax, [edi]
-	cmp		edx, 0
-	je		EvenArray
-	jmp		DisplayNum
+	div	ebx
+	shl	eax, 2
+	add	edi, eax
+	mov	eax, [edi]
+	cmp	edx, 0
+	je	EvenArray
+	jmp	DisplayNum
 ;------------------------------------------------find the median for an even array
 EvenArray:
-	sub		edi, 4
-	add		eax, [edi]
-	div		ebx
+	sub	edi, 4
+	add	eax, [edi]
+	div	ebx
 DisplayNum:
 	call	WriteDec
 	call	CrLf
 	call	CrLf
 ;------------------------------------------------reset ebp and return with 1 message and 2 variables
-	pop		ebp
-	ret		12
+	pop	ebp
+	ret	12
 
 displayMedian ENDP
 
 
 ;-----------------------------------------------;
-; PROC:			displayList						;
-; PURPOSE:		displays current array			;
-; PARAMETERS:	title, userArray, userNum		;
-; RETURNS:		none							;
+; PROC:		displayList			;
+; PURPOSE:	displays current array		;
+; PARAMETERS:	title, userArray, userNum	;
+; RETURNS:	none				;
 ;-----------------------------------------------;
 
 displayList PROC
 ;------------------------------------------------set up stack frame
 	push	ebp
-	mov		ebp, esp
+	mov	ebp, esp
 ;------------------------------------------------access title and display
-	mov		edx, [ebp+16]
+	mov	edx, [ebp+16]
 	call	WriteString
 	call	CrLf
 ;------------------------------------------------set up userNum as loop count, edx as element counter, and set up userArray
-	mov		esi, [ebp+12]
-	mov		ecx, [ebp+8]
-	mov		ebx, 0
+	mov	esi, [ebp+12]
+	mov	ecx, [ebp+8]
+	mov	ebx, 0
 ;------------------------------------------------get current element, display, and set up next element
 More:
-	mov		eax, [esi]
+	mov	eax, [esi]
 	call	WriteDec
-	add		esi, 4
+	add	esi, 4
 ;------------------------------------------------Count the number of integers for each line
-	inc		ebx
-	cmp		ebx, 10
-	je		NewLine
+	inc	ebx
+	cmp	ebx, 10
+	je	NewLine
 	mov     al, TAB
-    call    WriteChar
-	jmp		LoopAgain
+    	call    WriteChar
+	jmp	LoopAgain
 ;------------------------------------------------Add new line if 10 numbers in current line
 NewLine:
 	call	CrLf
-	mov		ebx, 0
+	mov	ebx, 0
 ;------------------------------------------------Loop again
 LoopAgain:
 	loop	More
 ;------------------------------------------------reset ebp and return with 1 message and 2 variables
 	call	CrLf
 	call	CrLf
-	pop		ebp
-	ret		12
+	pop	ebp
+	ret	12
 
 displayList ENDP
 
 
 ;-----------------------------------------------;
-; PROC:			farewell						;
-; PURPOSE:		displays goodbye message		;
-; PARAMETERS:	none							;
-; RETURNS:		none							;
+; PROC:		farewell			;
+; PURPOSE:	displays goodbye message	;
+; PARAMETERS:	none				;
+; RETURNS:	none				;
 ;-----------------------------------------------;
 
 farewell PROC
 ;------------------------------------------------set up stack frame
 	push	ebp
-	mov		ebp, esp
+	mov	ebp, esp
 ;------------------------------------------------Verify results with programmer name
-	mov		edx, [ebp+12]
+	mov	edx, [ebp+12]
 	call	WriteString
 	call	CrLf
 ;------------------------------------------------Say goodbye
-	mov		edx, [ebp+8]
+	mov	edx, [ebp+8]
 	call	WriteString
 	call	CrLf
 	call	CrLf
-	pop		ebp
-	ret		8
+	pop	ebp
+	ret	8
 
 farewell ENDP
 
